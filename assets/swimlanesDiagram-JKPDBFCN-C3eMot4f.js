490fc8be198f001362ea521c00d5f60106ab2c3c
minecraftbucuo <117921717+minecraftbucuo@users.noreply.github.com>
2026-09-06 16:35:50 +0000

    Deploying to page from @ minecraftbucuo/website@69b828022c2625f35c9fa184fe15b0c85925effe 🚀

---
+++ assets/swimlanesDiagram-JKPDBFCN-C3eMot4f.js
@@ -0,0 +1,8 @@
+import{T as t,f as o}from"./flowDiagram-YHGXBVSY-CDGgzKB9.js";import"./chunk-AZZRMDJM-BxFR_A4N.js";import"./chunk-RCXAM2GK-Bey4cj0T.js";import"./chunk-VTHDQ7BV-C9NosoRZ.js";import{m as e}from"./mermaid.esm.min-BrG79khG.js";import"./chunk-EICP7K5X-CtkBxSJI.js";import"./app-eU9pFWIm.js";var a=e(r=>`${o(r)}
+  .swimlane.cluster rect {
+    stroke: ${r.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=a,u=t({defaultLayout:"swimlane",styles:m});export{u as diagram};
